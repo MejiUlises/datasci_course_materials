35a49c9301ce6103312c29dd34f6ccf506edd0a2
MejiUlises <jonhylargo@gmail.com>
2014-07-11 22:30:54 -0500

    I believe this is it

--- assignment1/happiest_state.py
+++ assignment1/happiest_state.py
@@ -2,15 +2,88 @@ import sys
 import json
 import operator
 
+states = {'AK': 'Alaska',
+		  'AL': 'Alabama',
+		  'AR': 'Arkansas',
+		  'AS': 'American Samoa',
+		  'AZ': 'Arizona',
+		  'CA': 'California',
+		  'CO': 'Colorado',
+		  'CT': 'Connecticut',
+		  'DC': 'District of Columbia',
+		  'DE': 'Delaware',
+		  'FL': 'Florida',
+		  'GA': 'Georgia',
+		  'GU': 'Guam',
+		  'HI': 'Hawaii',
+		  'IA': 'Iowa',
+		  'ID': 'Idaho',
+		  'IL': 'Illinois',
+		  'IN': 'Indiana',
+		  'KS': 'Kansas',
+		  'KY': 'Kentucky',
+		  'LA': 'Louisiana',
+		  'MA': 'Massachusetts',
+		  'MD': 'Maryland',
+		  'ME': 'Maine',
+		  'MI': 'Michigan',
+		  'MN': 'Minnesota',
+		  'MO': 'Missouri',
+		  'MP': 'Northern Mariana Islands',
+		  'MS': 'Mississippi',
+		  'MT': 'Montana',
+		  'NA': 'National',
+		  'NC': 'North Carolina',
+		  'ND': 'North Dakota',
+		  'NE': 'Nebraska',
+		  'NH': 'New Hampshire',
+		  'NJ': 'New Jersey',
+		  'NM': 'New Mexico',
+		  'NV': 'Nevada',
+		  'NY': 'New York',
+		  'OH': 'Ohio',
+		  'OK': 'Oklahoma',
+		  'OR': 'Oregon',
+		  'PA': 'Pennsylvania',
+		  'PR': 'Puerto Rico',
+		  'RI': 'Rhode Island',
+		  'SC': 'South Carolina',
+		  'SD': 'South Dakota',
+		  'TN': 'Tennessee',
+		  'TX': 'Texas',
+		  'UT': 'Utah',
+		  'VA': 'Virginia',
+		  'VI': 'Virgin Islands',
+		  'VT': 'Vermont',
+		  'WA': 'Washington',
+		  'WI': 'Wisconsin',
+		  'WV': 'West Virginia',
+		  'WY': 'Wyoming'}
 
-def rate_tweet(x):
-	score = 0
-	word = []  #list of words of a single tweet x
 
-	if u'text in x':
-		msg = x[u'text'].encode("utf-8")
-		print msg
-	return score
+def clean_tweet(x):
+	x = x.replace("!", "")
+	x = x.replace("?", "")
+	x = x.replace(",", "")
+	x = x.replace(".", "")
+	x = x.strip()
+	return x
+
+
+def rate_tweet(x, dicti):
+	tweet_score = 0
+	words = []  # list of words of a single tweet x
+
+	msg = x[u'text'].encode("utf-8")  # get the message of the tweet
+	msg = clean_tweet(msg)	#clean the tweet a little
+	words = msg.split(" ")
+
+	for word in words:
+		if word in dicti:
+			tweet_score += dicti[word]
+	#print tweet_score
+
+	return tweet_score
 
 
 def main():
@@ -21,7 +94,7 @@ def main():
 	happiest_state = {}  # Empty dictionary with the occurrence of each hashtag
 	scores = {}
 
-	#We fill up the dictionary with the terms and scores in the AFINN-111.txt
+	# We fill up the dictionary with the terms and scores in the AFINN-111.txt
 	for line in sent_file:
 		term, score = line.split("\t")
 		scores[term] = int(score)
@@ -30,7 +103,7 @@ def main():
 	for line in tweet_file:
 		tweets.append(json.loads(line))  # Each tweet is parsed from JSON to a dictionary
 
-	#print scores
+	# print scores
 
 	for tweet in tweets:
 		if u'place' in tweet:
@@ -41,8 +114,19 @@ def main():
 					lugar[0] = str(lugar[0]).strip()
 					lugar[1] = str(lugar[1]).strip()
 					if lugar[1] in states.keys():
-						rate_tweet(tweet)
+						tweet_score = rate_tweet(tweet, scores)
+						if lugar[1] in happiest_state:
+							happiest_state[lugar[1]] += tweet_score
+						else:
+							happiest_state[lugar[1]] = tweet_score
+					elif lugar[0] in states.values():
+						tweet_score = rate_tweet(tweet, scores)
+						if states.keys()[states.values().index(lugar[0])] in happiest_state:
+							happiest_state[states.keys()[states.values().index(lugar[0])]] += tweet_score
+						else:
+							happiest_state[states.keys()[states.values().index(lugar[0])]] = tweet_score
 
+	print str(happiest_state.keys()[max(happiest_state.values())]) + " " + str(max(happiest_state.values()))
 
 if __name__ == '__main__':
 	main()
